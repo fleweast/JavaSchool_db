ada63af50ee318f982a294824e319c44faf6492a
Elena <igorevna737@gmail.com>
2021-08-19 20:22:08 +0300

    realized date convertor

---
+++ src/main/java/lab5/Convertor.java
@@ -0,0 +1,16 @@
+package lab5;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Convertor {
+    SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+
+    Date convertStringToDate(String date) throws ParseException {
+        return formatter.parse(date);
+    }
+    String convertDateToString(Date date) {
+        return formatter.format(date);
+    }
+}
---
+++ src/main/java/lab5/Main.java
@@ -0,0 +1,15 @@
+package lab5;
+
+import java.text.ParseException;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) throws ParseException {
+        Date date = new Date();
+        Convertor convertor = new Convertor();
+        String dateString = convertor.convertDateToString(date);
+        Date dateDate = convertor.convertStringToDate("20/08/2021");
+        System.out.println(dateString);
+        System.out.println(dateDate);
+    }
+}
