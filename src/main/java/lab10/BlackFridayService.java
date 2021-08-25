8bbba45481f3c78ad021cd440c860756a8f25ecf
Elena <igorevna737@gmail.com>
2021-08-25 10:26:41 +0300

    Stream

---
+++ src/main/java/lab10/BlackFridayService.java
@@ -0,0 +1,26 @@
+package lab10;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class BlackFridayService {
+
+    public static void printBlackFridayPerYearSorted(int startYear, int endYear) {
+
+        Stream.iterate(LocalDate.of(startYear, 1, 13),
+                        date -> date.getYear() <= endYear, date -> date.plusMonths(1))
+                .filter(date -> date.getDayOfWeek() == DayOfWeek.FRIDAY)
+                .collect(Collectors.groupingBy(LocalDate::getYear, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.<Integer, Long>comparingByValue(Long::compare).reversed())
+                .forEach(System.out::println);
+    }
+
+    public static void main(String[] args) {
+        printBlackFridayPerYearSorted(2001, 2009);
+    }
+}
