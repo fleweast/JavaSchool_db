040a4c6b35a8a54f9a6d2b9f01959db855c6fdfd
Elena <igorevna737@gmail.com>
2021-08-27 13:32:33 +0300

    learn Spring

---
+++ src/main/java/real_spring/ShakespearQuoter.java
@@ -0,0 +1,19 @@
+package real_spring;
+
+import lombok.Setter;
+import my_spring.InjectRandomInt;
+
+public class ShakespearQuoter implements Quoter {
+    @Setter
+    private String message;
+
+    @InjectRandomInt(min = 3, max = 5)
+    private int repeat;
+
+    @Override
+    public void sayQuote() {
+        for (int i = 0; i < repeat; i++) {
+            System.out.println(message);
+        }
+    }
+}
